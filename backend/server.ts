62f6efac652e4d3d3edd0d34a9c02ee8788edc9e
maddo <moritz-10@web.de>
2025-06-13 14:24:37 +0200

    added testdata

--- backend/server.ts
+++ backend/server.ts
@@ -6,13 +6,22 @@ import { typeDefs } from './src/graphql/schema';
 import { resolvers } from './src/graphql/resolvers';
 
 const app = express();
+
 app.use(cors({
-  origin: 'http://localhost:5173'  
+  origin: 'http://localhost:5173'
 }));
 
+// Root-Route (optional)
+app.get('/', (req, res) => {
+  res.send('API Server läuft!');
+});
+
+// REST-API-Routen registrieren
+app.use('/api/users', restRouter);
 
 const server = new ApolloServer({ typeDefs, resolvers });
 await server.start();
 server.applyMiddleware({ app, path: '/graphql' });
 
 app.listen(4000, () => console.log('Server läuft auf http://localhost:4000'));
+
